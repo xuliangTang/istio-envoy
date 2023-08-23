b5bd32ab3167f7d60e62d94f8dbbd7ff9de19a37
xuliangTang <1297441127@qq.com>
2023-08-23 16:16:14 +0800

    feat: 封装控制面

---
+++ mygateway/test/test_client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	envoy_config_route_v3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
+	routeservice "github.com/envoyproxy/go-control-plane/envoy/service/route/v3"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"time"
+)
+
+func main() {
+	gopts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+	addr := "localhost:19000"
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, addr, gopts...)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// 创建CDS grpc服务客户端
+	client := routeservice.NewRouteDiscoveryServiceClient(conn)
+	req := &discovery.DiscoveryRequest{
+		Node: &envoy_config_core_v3.Node{
+			Id: "test1",
+		},
+	}
+	rsp, err := client.FetchRoutes(context.Background(), req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// 获取resources节点内容
+	getResource := rsp.GetResources()[0]
+	// 反序列化为route对象
+	routeConfig := &envoy_config_route_v3.RouteConfiguration{}
+	err = getResource.UnmarshalTo(routeConfig)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(routeConfig)
+}
---
+++ mygateway/utils/resources.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+	route "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/cache/types"
+	"github.com/envoyproxy/go-control-plane/pkg/cache/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/resource/v3"
+	"istio-envoy/mygateway/tpls"
+	"log"
+)
+
+const input = `
+{
+   "listen_port": 8080
+}
+`
+
+// GenerateSnapshot 解析cue生成快照
+func GenerateSnapshot(version string) *cache.Snapshot {
+	lis := &listener.Listener{}
+	err := tpls.NewTplGenerator[*listener.Listener]().
+		GetOutput(input, "listener", false, lis)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	routeConfig := &route.RouteConfiguration{}
+	err = tpls.NewTplGenerator[*route.RouteConfiguration]().
+		GetOutput(input, "route", false, routeConfig)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// 构建snapshot
+	snap, _ := cache.NewSnapshot(version,
+		map[resource.Type][]types.Resource{
+			resource.RouteType:    {routeConfig},
+			resource.ListenerType: {lis},
+		},
+	)
+	return &snap
+}
